2b566bdb2631401ff73db959a3bc92a88e3b093b
matayoshi1307 <166684154+matayoshi1307@users.noreply.github.com>
2024-06-06 16:42:38 +0900

    first

---
+++ HelloController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public User hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new User();
+    }
+}
+
+
+
+
+
